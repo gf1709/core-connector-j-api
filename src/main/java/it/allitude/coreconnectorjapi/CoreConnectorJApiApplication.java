8fb8ced357cc9b58d7360f4a4adad456ec7f4d7b
fc0382 <gregorio.fracella@allitude.it>
2024-05-10 15:37:12 +0200

    feat:first commit

---
+++ src/main/java/it/allitude/coreconnectorjapi/CoreConnectorJApiApplication.java
@@ -0,0 +1,13 @@
+package it.allitude.coreconnectorjapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreConnectorJApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreConnectorJApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/allitude/coreconnectorjapi/CoreConnectorJApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.allitude.coreconnectorjapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreConnectorJApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
